b18ab54bf15d7f8c1cd28e5425c88e471dcef10a
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-17 21:12:07 +0100

    set background images on main page

--- src/components/featuredItem/FeaturedItem.js
+++ src/components/featuredItem/FeaturedItem.js
@@ -7,10 +7,10 @@ const FeaturedItem = (props) => {
       <div className="featured_item">
         <div className="item_text">
           {props.children}
-          <img src={props.small_image_1} alt="" />
-          <img src={props.small_image_2} alt="" />
+          <img className="small_image_1" src={props.small_image_1} alt="" />
+          <img className="small_image_2" src={props.small_image_2} alt="" />
         </div>
-        <img src={props.image} alt="hero" />
+        <img className="food" src={props.image} alt="hero" />
       </div>
     </div>
   );
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,21 +7,33 @@ const Main = (props) => {
     <div className="main_page">
       <Header />
       <main className="featured_items_container">
-        <FeaturedItem small-image_1="" small-image_2="" image="./image_1.jpg">
+        <FeaturedItem
+          small_image_1="./spaghetti.svg"
+          small_image_2="./groceries.svg"
+          image="./image_1.jpg"
+        >
           <h2>Choose your meals</h2>
           <p>
             Our chef-designed recipes include balanced Mediterranean meals,
             quick one-pan dinners, and top-rated customer favorites.
           </p>
         </FeaturedItem>
-        <FeaturedItem small-image_1="" small-image_2="" image="./image_2.jpg">
+        <FeaturedItem
+          small_image_1="./juice.svg"
+          small_image_2="cheese.svg"
+          image="./image_2.jpg"
+        >
           <h2>Unpack your box</h2>
           <p>
             We guarantee the freshness of all our ingredients and deliver them
             in an insulated box right to your door.
           </p>
         </FeaturedItem>
-        <FeaturedItem small-image_1="" small-image_2="" image="./image_3.jpg">
+        <FeaturedItem
+          small_image_1="./vegetables.svg"
+          small_image_2="./carrot.svg"
+          image="./image_3.jpg"
+        >
           <h2>Create magic</h2>
           <p>
             Following our step-by-step instructions you'll experience the magic
