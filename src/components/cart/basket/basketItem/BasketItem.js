9588ca6f808613547e434c2a27adc234edd6ffb9
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-19 21:31:03 +0100

    basket item stylings

--- src/components/cart/basket/basketItem/BasketItem.js
+++ src/components/cart/basket/basketItem/BasketItem.js
@@ -3,27 +3,33 @@ import "./BasketItem.scss";
 
 const BasketItem = () => {
   return (
-    <div className="basket_item">
-      <img src="./images/burger_1.png" alt="" />
-      <div className="basket_item_info">
+    <>
+      <div className="remove">
         <button className="remove_btn" alt="remove button">
           remove
         </button>
-        <div className="serving_info">
-          <p className="servings_quantity">2 servings</p>
-          <p className="items_quantity">2 burgers</p>
-        </div>
-        <p className="basket_item_description">
-          What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s when an unknown printer took a galley
-        </p>
-        <div className="quantity">
-          <input type="number" min="1" max="20" />
-          <span>£15</span>
+      </div>
+      <div className="basket_item">
+        <img src="./images/burger_1.png" alt="" />
+
+        <div className="basket_item_info">
+          <div className="serving_info">
+            <p className="servings_quantity">2 servings</p>
+            <p className="items_quantity">2 burgers</p>
+          </div>
+          <p className="basket_item_description">
+            What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s when an unknown printer
+            took a galley
+          </p>
+          <div className="quantity">
+            <input type="number" min="1" max="20" />
+            <span>£15</span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
