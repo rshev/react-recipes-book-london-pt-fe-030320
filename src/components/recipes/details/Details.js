0d0eafa18dbf3381a6821a69c53acf925074d002
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-19 16:01:22 +0100

    details component

--- src/components/recipes/details/Details.js
+++ src/components/recipes/details/Details.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Details.scss";
 
 const Details = () => {
   return (
@@ -9,12 +10,19 @@ const Details = () => {
           <span>55 min</span>
         </div>
         <div className="item_info">
-          <span className="item_info_1">2 servings</span>
-          <span className="item_info_2">820 cal/serving</span>
+          <span className="servings">2 servings</span>
+          <span className="calories">820 cal/serving</span>
         </div>
-        {/* <div className="item_info"></div> */}
+        <p className="item_description">
+          Plant based What is Lorem Ipsum Lorem Ipsum is simply dummy text of
+          the printing and typesetting industry Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s when an unknown
+          printer took a galley of type and scrambled it to make a type specimen
+          book it has?
+        </p>
+        <button className="yellow_button">Add to basket</button>
       </div>
-      <div>
+      <div className="recipe_pic">
         <img src="./images/burger_large.jpg" alt="" />
       </div>
     </div>
