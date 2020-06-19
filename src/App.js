37298bba0580d4b09dfee32868a8628c816a12aa
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-19 16:03:01 +0100

    added mixins

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Nav from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
 import Recipes from "./components/recipes/Recipes";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route path="/" component={Main} exact />
         <Route path="/recipes" component={Recipes} />
+        <Route path="/cart" component={Cart} />
       </Switch>
       <Footer />
     </div>
