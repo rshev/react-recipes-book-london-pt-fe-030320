f10b0f95860f47d1ca6a11df18b7b5b50a45d437
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-19 16:03:42 +0100

    basket item comp

---
+++ src/components/cart/basket/basketItem/BasketItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./BasketItem.scss";
+
+const BasketItem = () => {
+  return (
+    <div className="basket_item">
+      <img src="" alt="" />
+      <div className="basket_item_info">
+        <p>Servings</p>
+        <p>Burgers</p>
+        <p>
+          What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book it has?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BasketItem;
