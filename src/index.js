346ef2f42c687e7f95061dd2f2acf708d2421ab9
Oksana Kostenko <kseniavlad31@gmail.com>
2020-06-20 19:04:46 +0100

    scroll to top

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./logic/store";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
